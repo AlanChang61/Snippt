8af480dac22224546858ddb2aa2ebb2d6776af12
Alan Chang <alanchang61@gmail.com>
2020-02-16 08:13:12 -0800

    UI design and app icons

---
+++ Snippt/Snippt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snippt
+//
+//  Created by Student on 2/16/20.
+//  Copyright © 2020 Alan Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
