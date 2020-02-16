4e3e77f3fe2a8e10557ac9ec02288bc2c01aa981
Alan Chang <alanchang61@gmail.com>
2020-02-16 08:22:38 -0800

    Added view controllers

---
+++ Snippt/Snippt/AddViewController.swift
@@ -0,0 +1,102 @@
+//
+//  AddViewController.swift
+//  Snippt
+//
+//  Created by Student on 2/16/20.
+//  Copyright © 2020 Alan Chang. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+        //IBOutlets
+        @IBOutlet weak var titleTextField: UITextField!
+        @IBOutlet weak var codeTextView: UITextView!
+        @IBOutlet weak var cancelButton: UIBarButtonItem!
+        @IBOutlet weak var saveButton: UIBarButtonItem!
+        
+        //Data for this variable will be received from ViewController.swift
+        var unEditedCode:String?
+        //Global variable for text view and text field
+        var textViewField = ["view": false, "field":false]
+        //Code Formatter struct
+        var formatter:CodeFormatter = CodeFormatter()
+        //Shared model
+        var singleton = SavedCodeDataModel.singleton
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            saveButton.isEnabled = false
+            codeTextView.delegate = self
+            titleTextField.delegate = self
+            codeTextView.text = unEditedCode;
+        }
+        
+        
+        @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+            if (saveButton.isEnabled) {
+                let code = codeTextView.text ?? ""
+                let title = titleTextField.text ?? ""
+                singleton.insert(title: title, code: code, at: singleton.numSavedCodes()-1)
+               dismiss(animated: true, completion: nil)
+           }
+        }
+        @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+            titleTextField.text = ""
+            codeTextView.text = ""
+            
+            
+            // close view
+            dismiss(animated: true, completion: nil)
+        }
+    
+        //Functions below do two things 1) Make keyboard go away appropriately, 2) update text in the textView/Field
+        func textFieldShouldReturn (_ textField: UITextField) -> Bool {
+            if textField == titleTextField {
+                titleTextField.resignFirstResponder()
+                codeTextView.becomeFirstResponder()
+                enableSaveButton()
+            }
+            enableSaveButton()
+            return true
+        }
+        
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+      
+            if let text = textView.text, let textRange = Range(range, in: text) {
+                
+                let updatedText = text.replacingCharacters(in: textRange, with: text)
+                
+                if updatedText.count > 0 {
+                    enableSaveButton()
+                }
+                else if updatedText.count == 0 {
+                    enableSaveButton()
+                }
+            }
+            return true
+        }
+    
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+             if let text = textField.text, let textRange = Range(range, in: text) {
+                 
+                 let updatedText = text.replacingCharacters(in: textRange, with: string)
+                 
+                 if updatedText.count > 0 && textField == titleTextField {
+                     enableSaveButton()
+                 }
+                 else if updatedText.count == 0 && textField == titleTextField  {
+                     enableSaveButton()
+                 }
+                 
+             }
+             return true
+         }
+    
+        //Enable save button if both fields are filled out
+        func enableSaveButton() {
+            saveButton.isEnabled = titleTextField.text != nil && codeTextView.text != nil
+        }
+}
---
+++ Snippt/Snippt/SavedCodeDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  SavedCodeDataModel.swift
+//  Snippt
+//
+//  Created by Student on 2/16/20.
+//  Copyright © 2020 Alan Chang. All rights reserved.
+//
+
+import Foundation
